169c94d4398ffad585bbef2200fc3016a6b23b7f
Matt <127mwilliams@frhsd.com>
2026-03-16 12:51:25 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let favoriteMovies = ["1917", "The Peanut Butter Falcon"];
+const hobbies = new Array("Biking", "Gaming", "Gym");
+const luckyNumbers = new Array(23, 1, 11);
+
+let fortune = ["yes", "no", "maybe", "askagain"];
+
+
+fortune[0] = "absolutely!";
+console.log(fortune[0]);
+
+fortune[3] = "sybau";
+console.log(fortune[3]);
+
+console.log(fortune.length);
+
+const superLong = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13 ,14, 15];
+
+console.log(superLong.length);
+
+superLong[superLong.length - 1] = 100000;
